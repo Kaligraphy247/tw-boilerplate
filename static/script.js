09c456c0f874cb8f874d7c33573ed495249bdfc9
James Ononogbu <42041008+Kaligraphy247@users.noreply.github.com>
2022-09-30 18:16:01 +0900

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,21 @@
+// alert("JS is present")
+
+console.log("JS is present")
+
+// mobileView = document.getElementById("mobile");
+// desktopView = document.getElementById("normal"); 
+// console.log(mobileView,  desktopView);
+// mobileView.style.display = 'none';
+
+// function switchToMobileView() {
+//     if (mobileView.style.display = 'none') {
+//         mobileView.style.display = '';
+//     }
+// }
+
+// function switchToDesktopView() {
+//     if (desktopView.style.display != 'none') {
+//         mobileView.style.display = 'none';
+//     }
+// }
+
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './**{html, js, css}'
+    ],
+    theme: {
+      extend: {},
+      // screens: {
+      //   'xxs': '400px', // minwith
+
+      // },
+    },
+    plugins: [],
+  }
+  
