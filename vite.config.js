fe2d18fa85168863c1dc6f17e1fc10f26583b6f3
James Ononogbu <42041008+Kaligraphy247@users.noreply.github.com>
2023-01-04 22:13:28 +0100

    Instructions for npm run build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
+                  // add al the pages required here.
+                 // use the format below to add your page.
+                // see https://vitejs.dev/guide/build.html#multi-page-app for more info
                 main: resolve(__dirname, 'index.html'),
                 // admin: resolve(__dirname, 'admin.html'),
             },
